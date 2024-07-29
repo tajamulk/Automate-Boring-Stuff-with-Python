89e302754b7c315167dff75dd2aa29a2336ea3f0
Tajamul Khan <tajamulk@gmail.com>
2024-07-30 00:01:17 +0530

    Update 1. Python Basics.py

--- 1. Python Basics.py
+++ 1. Python Basics.py
@@ -1,11 +1,11 @@
 # Lesson 1 : Getting Help
-Explain what you are trying to do, not just what you did.
+'''Explain what you are trying to do, not just what you did.
 If you get an error message, specify the point at which the error happens. (Including the line number.)
 Explain what you’ve already tried to do to solve your problem.
-List the version of Python you’re using.
+List the version of Python you’re using.'''
 
-Lesson 2: Getting Started
-An instruction that evaluates to a single value is an expression. An instruction that doesn't is a statement.
+# Lesson 2: Getting Started
+'''An instruction that evaluates to a single value is an expression. An instruction that doesn't is a statement.
 IDE is an editor. Jupyter is specifically for Data science and scientific computing while VS code is for software development.
 The interactive shell window has the >>> prompt.
 Data types:
@@ -14,4 +14,4 @@ Character: String
 Bool
 Strings hold text and begin and end with quotes: ‘Hello world!'
 Values can be stored in variables: spam = 42
-Variables can be used anywhere values can be used in expressions: spam + 1
+Variables can be used anywhere values can be used in expressions: spam + 1'''
